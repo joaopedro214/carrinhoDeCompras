41cfdb33cff2a6ee990f47bd743a88abf7252b41
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2024-12-29 01:39:01 -0300

    Adiciona funcionalidade de limpar

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
+let precoTotal;
+limpar();
+
 function adicionar () {
     let produto = document.getElementById('produto').value; // Obtém o valor selecionado no elemento <select> com ID 'produto'.
-    let listaProdutos = document.querySelector('#lista-produtos'); // Seleciona o elemento <section> onde os produtos serão listados.
-    let quantidade = document.getElementById('quantidade').value; // Obtém o valor inserido no elemento <input> com ID 'quantidade'.
+    let nomeProduto = produto.split('-')[0]; // Quebra a string em array e pega somente o valor da posição 0.
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let precoSubtotal = quantidade * valorUnitario;  // Calcula o preço subtotal.
+    let carrinho = document.getElementById('lista-produtos');
 
-    if (produto && quantidade > 0) {
-        let novoItem = document.createElement('section');
-        novoItem.innerHTML = `<span class="texto-azul">${quantidade}</span> ${produto}`;
-        // Adiciona o novo elemento à lista de produtos no carrinho.
-        listaProdutos.appendChild(novoItem);
+    // Adiciona ao carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${precoSubtotal}</span></section>`;
 
-        atualizaTotal();
-    }
+    // Atualiza valor geral
+    precoTotal = precoTotal + precoSubtotal;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${precoTotal}`;
+    document.getElementById('quantidade').value = '';
+
+}
+
+function limpar () {
+    precoTotal = 0;
+    document.getElementById('quantidade').value = "";
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
 }
